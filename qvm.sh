c3dbcaaa59beb9c697d51124780f6b73979a4fbb
dwbfox <dwbfox@users.noreply.github.com>
2017-09-12 21:21:36 -0500

    Added backup option
    Added backup option

--- qvm.sh
+++ qvm.sh
@@ -2,40 +2,65 @@
 
 
 VIRSH_BIN=/usr/bin/virsh
-VERSION=0.1
+
 
 # Start VMs
 function startVMs {
     for vm in $("$VIRSH_BIN" list --all | sed 1,2d | cut -d' ' -f 7); do
-        virsh start $vm
+        $VIRSH_BIN start $vm
     done
 }
 
 
 function stopVMs {
     for vm in $("$VIRSH_BIN" list --all | sed 1,2d | cut -d' ' -f 7); do
-        virsh shutdown $vm
+        $VIRSH_BIN shutdown $vm
     done
 }
 
+function backupXMLs {
+   for vm in $("$VIRSH_BIN" list --all | sed 1,2d | cut -d' ' -f 7); do
+        echo "Backing up $vm to $1/$vm.xml"
+        $VIRSH_BIN dumpxml $vm > "$1/$vm.xml"
+   done;
+}
+
+
 if [ -z $1 ]; then
 cat <<EOF
-qvm.sh version ${VERSION}
+  start-vms.sh version 0.1
 
-Start and stop virtual maachines.
-Syntax: ./start-vms.sh [start|stop]
+  Start and stop virtual maachines.
+  Syntax: ./start-vms.sh [start|stop|backup]
+
+  start: starts all VMs
+  stop: stops all VMs
+  backup: backs up all VM domain XMLs to the
+          specified directory. Format:
+          backup <path>
 
 EOF
 exit
 fi
 
-if [ $1 == 'start' ]; then
+
+if [[ $1 == 'start' ]]; then
     echo "Starting virtual machines..."
    startVMs
 fi
 
 
-if [ $1 == 'stop' ]; then
+if [[ $1 == 'stop' ]]; then
     echo "Shutting down virtual machines..."
     stopVMs
+fi
+
+if [[ $1 == 'backup' ]]; then
+    if [[ ! -d $2 ]]; then
+        echo "Specified backup directory was not found :${2}"
+        exit 1
+    fi
+    echo "Backing up virtual machine domain XML..."
+    echo "Backup location: ${2}"
+    backupXMLs $2
 fi
