7bbdea31f61aeb9c83c514eb401c726cf8f266b7
dwbfox <contact@dbiru.com>
2017-12-30 22:00:13 -0600

    Restructured args, started implementing functionality
    Signed-off-by: dwbfox <contact@dbiru.com>

--- qvm.sh
+++ qvm.sh
@@ -27,32 +27,43 @@
 #   
 ###############################################################################
 
+# Remove before flight
+# set -e 
+# set -u
+# set -o pipefail
+# set -x
 
 
 
 show_usage() {
 cat <<EOF
-usage: qvm [options]
+usage: qvm [options] [command]
 
 Examples:
-    qvm -d centos-vm-01 -b /tmp/mydomainxmls     # Create a directory and backup the domain XML at the specified location
-    qvm -l                                       # List all running and stopped domains, equivalent to "virsh list --all"
-    qvm -r -d centos-vm-01                       # Gracefully reboots the specified domain
+    qvm backup -d centos-vm-01 -b /tmp/mydomainxmls     # Create a directory and backup the domain XML at the specified location
+    qvm list                                            # List all running and stopped domains, equivalent to "virsh list --all"
+    qvm reboot -d centos-vm-01                          # Gracefully reboots the specified domain
+
+List of commands:
+    backup                          backup domain XMLs, by default in the current directory. Can be combined 
+                                    with -b and -d to specify directories and specific domains to backup
+    list                            List all domains, analogous to running "virsh list --all". 
+                                    Can be combined with -d to list a specific domain
+    reboot                          Gracefully reboots all domains. Can be combined with -d to specify a domain to reboot
+    start                           Starts all domains. Can be combined with -d to specify a domain to start
 
 Command options:
-    -b [directory]       Backup all domain XMLs into the specified directory, can be combined with -d to specify a specific domain XML to backup
-    -d [domain]          The name of the domain to run a command on
-    -e                   Destroy/stop all running domains, can be combined with -d to specify a specific domain
-    -l                   List all domains, including ones that are shut down      
+    -b [directory]       Specify a directory
+    -d [domain]          Specify a domain
     -h                   Display this help
+    -o [file]            Output verbose logging to a specified file
     -i [path]            Virsh executable path, by default /usr/bin/virsh
-    -r                   Gracefully reboot (soft reboot) all running domains, can be combined with -d to specify a specific domain
-    -s                   Start all domains, can be combined with -d to specify a specific domain
     -v                   Enable verbose command outputs
 
 Quick Virsh Manager v0.5.1
 
 EOF
+    tput init
     exit 0
 }
 
@@ -70,6 +81,14 @@ start_vm() {
     fi
 }
 
+yes_or_exit() {
+    read -r  response
+    if [[ ! "$response" = "y" || ! "$response" = "Y" ]]; then
+        echo_error "Exiting...$response"
+        exit 1
+    fi
+}
+
 stop_vm() {
     for vm in $("$FLAG_VIRSH_BIN" list --all | sed 1,2d | cut -d' ' -f 7); do
         $FLAG_VIRSH_BIN shutdown "$vm"
@@ -77,19 +96,40 @@ stop_vm() {
 }
 
 backup_domain() {
-   for vm in $("$FLAG_VIRSH_BIN" list --all | sed 1,2d | cut -d' ' -f 6,7); do
-        echo_verbose "Backing up $vm to $1/$vm.xml"
-        $FLAG_VIRSH_BIN dumpxml "$vm" > "$1/$vm.xml"
-   done;
+
+    if [[ ! -z $FLAG_DOMAIN ]]; then
+        echo_success "Backing up ${FLAG_DOMAIN} to ${FLAG_DIRECTORY}"
+        if [[ ! -z $FLAG_DIRECTORY && -d $FLAG_DIRECTORY ]]; then
+            # Check if there are existing domain backups at this location
+            if [[ -f "${FLAG_DIRECTORY}/${FLAG_DOMAIN}.xml" ]]; then
+                echo_error "A file exists under "${FLAG_DIRECTORY}/${FLAG_DOMAIN}.xml". Do you want to continue and overwrite this file? [Y/N]"
+                yes_or_exit
+            fi
+            $FLAG_VIRSH_BIN dumpxml "$FLAG_DOMAIN" > "$FLAG_DIRECTORY/$FLAG_DOMAIN.xml"
+        else
+            $FLAG_VIRSH_BIN dumpxml "$FLAG_DOMAIN" > "./$FLAG_DOMAIN.xml"
+        fi
+    else
+       for vm in $("$FLAG_VIRSH_BIN" list --all | sed 1,2d | cut -d' ' -f 6,7); do
+            echo_verbose "Backing up VM"
+            if [[ ! -z $FLAG_DIRECTORY && -d $FLAG_DIRECTORY ]]; then
+                # Check if there are existing domain backups at this location
+                if [[ -f "${FLAG_DIRECTORY}/${FLAG_DOMAIN}.xml" ]]; then
+                    echo_error "A file exists under "${FLAG_DIRECTORY}/${FLAG_DOMAIN}.xml". Do you want to continue and overwrite this file? [Y/N]"
+                    yes_or_exit
+                fi
+                $FLAG_VIRSH_BIN dumpxml "$vm" > "$FLAG_DIRECTORY/$vm.xml"
+            else
+                $FLAG_VIRSH_BIN dumpxml "$vm" > "./$vm.xml"
+            fi
+       done;
+    fi
 }
 
 list_vm() {
     echo_verbose "${FUNCNAME[0]}() flag domain value: $FLAG_DOMAIN"
     if [[ ! -z "$FLAG_DOMAIN" ]]; then
-        $FLAG_VIRSH_BIN list --all | grep $FLAG_DOMAIN
-        if [[ ! "$?" -eq 0 ]]; then
-            echo "Lookup failed. Check if \"$FLAG_DOMAIN\" exists."
-        fi
+        $FLAG_VIRSH_BIN list --all | grep "$FLAG_DOMAIN"
     else
         $FLAG_VIRSH_BIN list --all
     fi
@@ -98,7 +138,7 @@ list_vm() {
 echo_verbose() {
     log="[$(date +'%Y-%m-%d %H:%m:%S')] -> $1"
     if [[ ! "$FLAG_VERBOSE" -eq 0 ]]; then
-        echo $log
+        echo "$log"
     fi
     if [[ ! -z "$FLAG_LOG_LOCATION" ]]; then
         echo "$log" >> "$FLAG_LOG_LOCATION"
@@ -106,45 +146,74 @@ echo_verbose() {
 }
 
 
+echo_error() {
+    tput setaf 1; printf "$1\n"; tput init
+}
+
+
+echo_success() {
+    tput setaf 2; printf "$1\n\n"; tput init
+}
+
 FLAG_VIRSH_BIN=/usr/bin/virsh
 FLAG_LOG_LOCATION=""
 FLAG_DOMAIN=""
 FLAG_DIRECTORY=""
 FLAG_VERBOSE=0
-while getopts "o:b:d:vesrlh" opt; do
-    case $opt in
+
+
+# Parse incoming options
+while getopts "i:vd:b:ho:" opt; do
+    case "${opt}" in
         b )
-            echo_verbose "Directory specified: $OPTARG"
             FLAG_DIRECTORY=$OPTARG
+            echo_verbose "Directory set to ${FLAG_DIRECTORY}"
             ;;
         d )
-            echo_verbose "Domain specified: $OPTARG"
             FLAG_DOMAIN=$OPTARG
-            ;;            
+            echo_verbose "Domain set to ${FLAG_DOMAIN}"
+            ;;
         h )
             show_usage
             ;;
-        l )
-            echo_verbose "-l, listing domains"
-            list_vm
+        i ) 
+            FLAG_VIRSH_BIN=$OPTARG
+            echo_verbose "Virsh executable path changed to ${FLAG_VIRSH_BIN}"
             ;;
         o )
-            echo_verbose "Logging location: $OPTARG"
             FLAG_LOG_LOCATION=$OPTARG
-            ;;
-        s ) 
-            echo_verbose "Starting domain..."
-            start_vm
             ;;
         v )
             FLAG_VERBOSE=1
-            echo_verbose "Verbose enabled"
+            echo_verbose "Verbose mode enabled"
             ;;
-
         \?)
-            echo_verbose "Unknown argument, calling show_usage()"
+            echo_error "Invalid option specified"
             show_usage
             ;;
     esac
 done
+shift "$((OPTIND-1))"
+
+case "${1}" in
+    backup )
+        echo_verbose "Backup command specified"
+        backup_domain
+        ;;
+    list )
+        echo_verbose "List command specified"
+        list_vm
+        ;;
+    reboot )
+        echo_verbose "Reboot command specified"
+        ;;
+    start ) 
+        echo_verbose "Start command specified"
+        ;;
+    * )
+        echo_error "Invalid command specified: $1"
+        show_usage
+        exit 1
+        ;;
+esac
 
